1bee77e4b5c7e3a8e8413a9d5cd976b756c08411
Mitja <mitja.kurki@gmail.com>
2019-08-19 17:45:12 -0400

    Updated docker file. Changed naming of output files: conditional summary stats does not include snps to avoid too long filenames and summary output contains the first condition snp name

--- scripts/do_condition_locus.py
+++ scripts/do_condition_locus.py
@@ -14,15 +14,16 @@ def run_cond(null, var_ratio, bgen,bgen_idx,sample, chr, start, stop, output, co
     cmd = f'{script} --bgenFile {bgen} --bgenFileIndex {bgen_idx} --sampleFile={sample} --minMAC {minmac}' \
             f' --GMMATmodelFile {null} --varianceRatioFile {var_ratio} --SAIGEOutputFile {output}'  \
             f' --condition {",".join(condition_on)} --rangestoIncludeFile tmp.region'
-    logging.info(f'Running command {cmd}') 
+    logging.info(f'Running command {cmd}')
     try:
         subprocess.check_call(cmd, shell=True)
     finally:
         os.system("rm tmp.region")
-    # currently saige output doubles variantid but does not give correct number of headers
-    p = subprocess.Popen(f'cat <(head -n 1 {output}) <(cut -f 1-3,5- -d" " {output} | tail -n+2) >  {output}_fix', shell=True, executable='/bin/bash')
-    p.wait()
-    os.system(f'mv {output}_fix {output}')
+
+
+def plot_locus(result_file):
+
+    pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run conditional saige until no significant results remaining")
@@ -39,23 +40,27 @@ if __name__ == '__main__':
     parser.add_argument('--out_prefix', action='store', default="", type=str, help='')
     parser.add_argument('--p_threshold', action='store', default=5e-8, type=float, help='')
     parser.add_argument('--min_mac', action="store", default=10, type=int)
-    parser.add_argument('--saige_script', action="store", default="step2_SPAtests.R",  )
+    parser.add_argument('--saige_script', action="store", default="step2_SPAtests.R")
+    parser.add_argument('--plot_locus', action="store_true", default=False )
+
     args = parser.parse_args()
-    
+
     logging.basicConfig(filename=f'{args.out_prefix+"_" if len(args.out_prefix)>0 else ""}{os.path.basename(args.null_file)}.conditional.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
 
-    out = f'{args.out_prefix+"_" if len(args.out_prefix)>0 else ""}{os.path.basename(args.null_file)}_{args.condition_snp}.conditional'
+    out = f'{args.out_prefix+"_" if len(args.out_prefix)>0 else ""}{os.path.basename(args.null_file)}_1.conditional'
     cond_snps = [args.condition_snp]
     run_cond(args.null_file, args.variance_ratio_file, args.bgen_file, args.bgen_index, args.sample_file,args.chr,
              args.start, args.stop, out, cond_snps, args.min_mac, args.saige_script)
 
+    if args.plot_locus:
+        plot_locus(out)
     columns = ["SNPID","BETA","SE","p.value","BETA_cond","SE_cond","p.value_cond"]
     res = pd.read_csv(out, sep=" ")
     r = res.loc[ (~res['SNPID'].isin(cond_snps)) & (res["p.value_cond"] < args.p_threshold),
                     columns].sort_values(by=["p.value_cond"])
 
-    summary_out = f'{args.out_prefix+"_" if len(args.out_prefix)>0 else ""}{os.path.basename(args.null_file)}_independent.snps'
-    
+    summary_out = f'{args.out_prefix+"_" if len(args.out_prefix)>0 else ""}{os.path.basename(args.null_file)}_{args.condition_snp}.independent.snps'
+
     tab="\t"
     with open( summary_out, 'w' ) as summary:
         summary.write("\t".join(columns) + "\tConditioned_on" + "\n"  )
@@ -64,9 +69,12 @@ if __name__ == '__main__':
         while not r.empty:
             summary.write(f'{tab.join( str(v) for v in r.iloc[0].values)}\t{",".join(cond_snps)}\n')
             cond_snps.append(r.iloc[0].SNPID)
-            out = f'{os.path.basename(args.null_file)}_{"-".join(cond_snps)}.conditional'
+            out = f'{args.out_prefix + "_" if len(args.out_prefix) > 0 else ""}{os.path.basename(args.null_file)}_{len(cond_snps)}.conditional'
+
             run_cond(args.null_file, args.variance_ratio_file, args.bgen_file, args.bgen_index, args.sample_file, args.chr,
                      args.start, args.stop, out, cond_snps, args.min_mac, args.saige_script )
+            if args.plot_locus:
+                plot_locus(out)
 
             res = pd.read_csv(out, sep=" ")
             r = res.sort_values(by=["p.value_cond"]).head().loc[
--- scripts/generate_conditional_analysis_config.py
+++ scripts/generate_conditional_analysis_config.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
 import argparse
 import pandas as pd
 import re
 from functools import partial
-
+import csv
+from pandas.api.types import is_string_dtype
 PHENO_TAG="{PHENO}"
+CHR_TAG="{CHR}"
 
 def row_conf(pheno, chr, start, stop, condition_snp, null_mask, var_ratio_mask, bgen_mask, sample_file, p_thr):
 
@@ -16,15 +18,72 @@ def row_conf(pheno, chr, start, stop, condition_snp, null_mask, var_ratio_mask, 
 
 
 def row_conf_line(row, null_mask, var_ratio_mask, bgen_mask, sample_file, p_thr):
-
+    chrom = row.chr.replace("chr","") if is_string_dtype(row.chr) else str(row.chr)
     return f'{re.sub(PHENO_TAG,row.pheno,null_mask,flags=re.IGNORECASE)}\t{re.sub(PHENO_TAG,row.pheno,var_ratio_mask,flags=re.IGNORECASE)}\t' \
-        f'{re.sub(PHENO_TAG,row.pheno,bgen_mask,flags=re.IGNORECASE)}.bgen\t{re.sub(PHENO_TAG,row.pheno,bgen_mask,flags=re.IGNORECASE)}.bgi\t' \
+        f'{re.sub(CHR_TAG,chrom,bgen_mask,flags=re.IGNORECASE)}.bgen\t{re.sub(CHR_TAG,chrom,bgen_mask,flags=re.IGNORECASE)}.bgi\t' \
         f'{sample_file}\t{row.chr}\t{row.start}\t{row.stop}\t{row.condition_snp}\t{p_thr}'
+
+
+
+def from_locus(args):
+    loci = pd.read_csv(args.locus_file, sep="\t")
+    loci.columns = map(str.lower, loci.columns)
+    r = loci.apply(partial(row_conf_line, null_mask=args.null_file_mask, var_ratio_mask=args.var_ratio_mask,
+                           bgen_mask=args.bgen_file_mask, sample_file=args.sample_file, p_thr=args.p_threshold), 1)
+    r.to_csv(args.output, index=False, header=True)
+
+
+
+def from_variants(args):
+
+    vars = pd.read_csv(args.snps_file, sep="\t", compression='gzip' if args.snps_file.endswith('gz') else 'infer',
+                       dtype={args.p_col:float})
+
+    vars = vars[ vars[args.p_col]<args.p_threshold ].sort_values(by=[args.pheno_col,args.p_col])
+    loc_width = args.locus_padding
+
+    loci = []
+
+    while len(vars.index) > 0:
+        top = vars.iloc[0]
+        loc_start = max(top[args.pos_col] - loc_width,1)
+        loc_end = top.pos + loc_width
+        loci.append( top )
+        ## we can sort by pheno and position and do the scanning linearly (n + n*log(n) )
+        vars = vars[ (vars[args.pheno_col] != top[args.pheno_col]) | (vars[args.chr_col]!= top[args.chr_col]) | (vars[args.pos_col]<loc_start) | ( vars[args.pos_col]>loc_end ) ]
+
+    loci_df = pd.concat(loci, axis=1).T.sort_values(by=[args.pheno_col, args.chr_col, args.pos_col] )
+
+    merged = []
+
+    def get_cols(s):
+        return pd.Series({"pheno": s.PHENO, "chr": s[args.chr_col],
+                   "start": s[args.pos_col] - loc_width, "stop": s[args.pos_col] + loc_width,
+                   "condition_snp":f'{s[args.chr_col]}_{s[args.pos_col]}_{s[args.ref_col]}_{s[args.alt_col]}', "pval":s[args.p_col] })
+
+    for i in range(0, len(loci_df.index) ):
+        curr = get_cols(loci_df.iloc[i])
+        if (len(merged) > 0 and merged[-1].pheno == curr.pheno and
+                curr.chr == merged[-1].chr and curr.start < merged[-1].stop):
+            win = curr if curr.pval < merged[-1].pval else merged[-1]
+            win.start = merged[-1].start
+            win.stop = curr.stop
+            merged[-1] = win
+        else:
+            merged.append(curr)
+
+    mergs = pd.concat(merged, axis=1).T
+
+    r = mergs.apply(partial(row_conf_line, null_mask=args.null_file_mask, var_ratio_mask=args.var_ratio_mask,
+                           bgen_mask=args.bgen_file_mask, sample_file=args.sample_file, p_thr=args.p_threshold), 1)
+
+    r.to_csv(args.output + ".merged", quoting=csv.QUOTE_NONE, index=False, header=False)
+    loci_df.to_csv(args.output,sep="\t", index=False, header=True)
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run conditional saige until no significant results remaining")
-    parser.add_argument('locus_file', action='store', type=str, help='file with 4 columns: pheno chr start stop condition_snp')
     parser.add_argument('null_file_mask', action='store', type=str, help='mask that maps to file location of null run file. '
                                                                          ' use {PHENO} as replacement for the pheno name( e.g. gs://data/R3.grm.v1-${PHENO}.rda)')
     parser.add_argument('var_ratio_mask', action='store', type=str,
@@ -42,11 +101,31 @@ if __name__ == '__main__':
     parser.add_argument('--p_threshold', action='store', type=float, default=5e-8,
                         help='p-value threshold for signal inclusion')
 
+    cmd_parsers = parser.add_subparsers(title="commands", help="Sub command help")
+    loci_cmd = cmd_parsers.add_parser('loci')
+    loci_cmd.add_argument('locus_file', action='store', type=str,
+                        help='file with 4 columns: pheno chr start stop condition_snp')
+    loci_cmd.set_defaults(func=from_locus)
+
+    vars_cmd = cmd_parsers.add_parser('vars')
+    vars_cmd.add_argument('snps_file', action='store', type=str,
+                          help='file giving candidate variants and phenotypes (e.g. all gw-sig variants) with at least 4 columns: PHENO chr pos ref alt p-value')
+    vars_cmd.add_argument('--locus_padding', action='store', type=int, default=1500000,
+                          help='Nucleotides +- from lead snp')
+
+    vars_cmd.set_defaults(func=from_variants)
+
+    vars_cmd.add_argument('--pheno_col', default="PHENO", type=str)
+    vars_cmd.add_argument('--chr_col', default="#chrom", type=str)
+    vars_cmd.add_argument('--pos_col', default="pos", type=str)
+    vars_cmd.add_argument('--ref_col', default="ref", type=str)
+    vars_cmd.add_argument('--alt_col', default="alt", type=str)
+    vars_cmd.add_argument('--p_col', default="pval", type=str)
+
+
     args = parser.parse_args()
 
-    loci = pd.read_csv(args.locus_file, sep="\t")
-    loci.columns = map(str.lower, loci.columns)
-    r = loci.apply( partial(row_conf_line, null_mask=args.null_file_mask, var_ratio_mask=args.var_ratio_mask,
-                        bgen_mask=args.bgen_file_mask, sample_file=args.sample_file, p_thr=args.p_threshold),1)
-    r.to_csv(args.output, index=False)
+    if not "func" in vars(args):
+        raise Exception("Command must be given")
 
+    args.func(args)
